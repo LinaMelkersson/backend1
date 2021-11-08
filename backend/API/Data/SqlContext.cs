ddaa0190efc99ece618f13672f6ba5cfa369b219
LinaMelkersson <lina.melkersson@learnet.se>
2021-11-08 14:19:33 +0100

    my first save

---
+++ backend/API/Data/SqlContext.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using API.Entities;
+
+#nullable disable
+
+namespace API.Data
+{
+    public partial class SqlContext : DbContext
+    {
+        public SqlContext()
+        {
+        }
+
+        public SqlContext(DbContextOptions<SqlContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Categorye> Categoryes { get; set; }
+        public virtual DbSet<OderLine> OderLines { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<SubCategorye> SubCategoryes { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserAddress> UserAddresses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Categorye>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<OderLine>(entity =>
+            {
+                entity.Property(e => e.Quantity).HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.UnitPrice).HasColumnType("money");
+
+                entity.HasOne(d => d.Orders)
+                    .WithMany(p => p.OderLines)
+                    .HasForeignKey(d => d.OrdersId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__OderLines__Order__36B12243");
+
+                entity.HasOne(d => d.Products)
+                    .WithMany(p => p.OderLines)
+                    .HasForeignKey(d => d.ProductsId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__OderLines__Produ__37A5467C");
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.OrderDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Status)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasDefaultValueSql("('Received')");
+
+                entity.Property(e => e.TotalAmount).HasColumnType("money");
+
+                entity.HasOne(d => d.UserAddresses)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.UserAddressesId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Orders__UserAddr__33D4B598");
+
+                entity.HasOne(d => d.Users)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.UsersId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Orders__UsersId__31EC6D26");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Price).HasColumnType("money");
+
+                entity.Property(e => e.ShortDescription)
+                    .IsRequired()
+                    .HasMaxLength(250);
+
+                entity.HasOne(d => d.SubCategoryes)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.SubCategoryesId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Products__SubCat__2F10007B");
+            });
+
+            modelBuilder.Entity<SubCategorye>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.Categoryes)
+                    .WithMany(p => p.SubCategoryes)
+                    .HasForeignKey(d => d.CategoryesId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__SubCatego__Categ__2C3393D0");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasIndex(e => e.Email, "UQ__Users__A9D10534AA9BE942")
+                    .IsUnique();
+
+                entity.Property(e => e.Admin).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Password).IsRequired();
+
+                entity.HasOne(d => d.UserAddresses)
+                    .WithMany(p => p.Users)
+                    .HasForeignKey(d => d.UserAddressesId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Users__UserAddre__29572725");
+            });
+
+            modelBuilder.Entity<UserAddress>(entity =>
+            {
+                entity.Property(e => e.Adress)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Zip)
+                    .IsRequired()
+                    .HasMaxLength(7)
+                    .IsUnicode(false)
+                    .IsFixedLength(true);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
